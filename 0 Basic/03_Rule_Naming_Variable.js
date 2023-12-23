24f6eae9ed2b14d8096d96ec68dd7992135ba2cf
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 00:57:10 +0530

    add rule for naming variables

---
+++ 0 Basic/03_Rule_Naming_Variable.js
@@ -0,0 +1,32 @@
+// rules for naming variables
+
+// you can not start with a number
+// example
+// 1value (incalid)
+// value1(valid)
+
+var value1= 10;
+console.log(value1);
+console.log(value1**2);
+
+// you can use only undersore _ or dollar symbol
+// first_name(valid)
+// _firstname (valid)
+
+_firstname ="abdul";
+console.log(_firstname);
+
+$firstname ="rehan";
+console.log($firstname);
+
+// first$name(valid)
+// $firstnme(valid)
+
+// you cannot use space
+var first_name ="salam"; //snake case writing
+var firstName = "rahman"; //camel case writing
+
+// first name (invalid)
+
+//convention
+// start with small letter and use camelCase
