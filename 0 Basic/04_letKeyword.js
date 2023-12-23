11e3d4b9f107dd755f650c117775cf518b4206c3
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 01:05:20 +0530

    add let kewords

---
+++ 0 Basic/04_letKeyword.js
@@ -0,0 +1,14 @@
+
+// let keywords
+// declare variable with let keywords
+
+let firstName = "samar";
+
+// we cant declare first name again in case of let
+// let firstName ="rehan";
+// but use can change value of first name
+
+firstName ="rehan";
+console.log(firstName);
+
+// block scope vs function scope (convered later)
