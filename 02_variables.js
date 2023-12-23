a2cb8cfa5415e89ae1d90e1db6dd42bad32dbc21
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 00:39:46 +0530

    add 02 variables.js

---
+++ 02_variables.js
@@ -0,0 +1,19 @@
+"use strict";
+// intro to variables
+
+// variables can store some information
+// we can use taht information later
+// we can change that information later
+
+// declare a variable
+
+var firstName = "Abdul";
+
+// use a variable
+
+console.log(firstName);
+
+// change value
+
+firstName = "Rahman";
+console.log(firstName);
