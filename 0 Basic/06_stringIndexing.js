a987900c4bcefb91354688640e3b892a7f27c003
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 01:29:16 +0530

    add string indexing

---
+++ 0 Basic/06_stringIndexing.js
@@ -0,0 +1,20 @@
+// String indexing
+
+let firstName="Abdul";
+
+// A b d u l
+// 0 1 2 3 4
+
+console.log(firstName[3]);
+
+// length of string 
+// firstName.lenght
+console.log(firstName.length);
+
+// last index : length - 1
+console.log(firstName[firstName.length-1]);
+
+
+var twoDigit = 23;
+console.log(twoDigit[0])
+console.log(twoDigit[1]);
