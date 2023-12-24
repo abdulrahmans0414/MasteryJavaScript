2605a818c183bca0de1a8e4b43d537ec34add8de
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 12:23:42 +0530

    add template string

---
+++ 0 Basic/10_templateStrings.js
@@ -0,0 +1,10 @@
+// template string 
+
+let age =22;
+let firstName= "Abdul";
+
+//"my name is Abdul and my age is 22"
+// let aboutMe = "my name is "+ firstName + " and my age is " + age; 
+
+let aboutMe= `my name is ${firstName} and my age is ${age}`;
+console.log(aboutMe);
