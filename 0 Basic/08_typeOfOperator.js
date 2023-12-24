faa9c9fc91df650efbf6b65c09bace1d920a9e2a
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 12:10:45 +0530

    add type of operator

---
+++ 0 Basic/08_typeOfOperator.js
@@ -0,0 +1,30 @@
+// typeof operator
+
+// data types (primitive data types)
+// string "Abdul"
+// num 2, 4 ,5 ,6
+// booleans
+// undefine 
+// null
+// BigInt
+// symbol
+
+let age = 22;
+let firstName = "Abdul";
+console.log(typeof age );
+console.log(typeof firstName );
+
+// convert number to string
+// 22 -> "22"
+age = age + " ";
+console.log(typeof (age)); //"22"
+
+// convert string to number 
+// "34" -> 34
+let myString = +"34";
+console.log(typeof myString); //34
+
+//let age =18;
+// age= String(age);
+// age= Number(age);
+// console.log(typeof(age));
