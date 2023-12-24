e968e45b250494d74fc1a998f2dc00f03c6fba5b
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 12:16:35 +0530

    add string concatenation

---
+++ 0 Basic/09_stringConcatenation.js
@@ -0,0 +1,15 @@
+// string concatenation 
+
+// let string1= "Abdul";
+// let string2 ="Rahman";
+
+// let fullName = string1 + " "+ string2;
+// console.log(fullName);
+
+let string1= "10";
+let string2 ="20";
+
+// string to number
+let newString = + string1 + + string2;
+console.log(typeof newString);
+console.log(newString);
