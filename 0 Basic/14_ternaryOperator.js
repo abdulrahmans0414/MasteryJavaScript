d7b9bae1b14d4ba62c81c0dea5f0fc99da9bc6df
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 14:18:38 +0530

    add ternary operatoer

---
+++ 0 Basic/14_ternaryOperator.js
@@ -0,0 +1,20 @@
+// ternary operator 
+
+// let age = 7;
+// let dirnk; //undefined
+
+// if(age>=5){
+//     drink="Coffee";
+// }
+// else{
+//     drink="milk";
+// }
+
+// console.log(drink);
+
+
+// ternary operator / conditional operator
+
+let age =7;
+let drink= age>=5 ? "coffee" : "milk";
+console.log(drink);
