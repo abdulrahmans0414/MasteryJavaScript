e873e9ddd80ad694b4f0754c89a7a27db3b3a475
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 14:13:34 +0530

    add if else condition and truthy , falsy value

---
+++ 0 Basic/13_if_else_truthyAndFalsyvalues.js
@@ -0,0 +1,40 @@
+// if else condition
+
+let age = 16;
+
+if(age>=18){
+    console.log("user cann play ddlc");
+}
+else{
+    console.log('user can play mario');
+}
+
+
+let num = 13;
+if(num%2===0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+
+// truthy and falsy value
+
+// falsy value 
+// false
+// ""
+// null
+// undefined
+// 0
+
+// truthy value
+// abc
+// 1 , -1
+
+let firstName="";
+if(firstName){
+    console.log(firstName);
+}
+else{
+    console.log("Empty");
+}
