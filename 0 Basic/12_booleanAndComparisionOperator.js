9224dbf44af30606288e3b33d614ea331f97c881
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 12:51:23 +0530

    add boolean and comparision operator

---
+++ 0 Basic/12_booleanAndComparisionOperator.js
@@ -0,0 +1,17 @@
+// boolean and comaparing operator
+
+// booleans
+// true, false
+
+let num1 ="7";
+let num2=7;
+
+// console.log(num2=>num1);
+
+// == vs  ===
+// console.log(num2==num1); // equals value
+// console.log((num1===num2)); //check datatype nad value
+
+// != vs !==
+console.log(num2 !=num1);
+console.log(num2 !==num1);
