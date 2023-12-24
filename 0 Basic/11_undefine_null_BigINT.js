944e5d6acc0720e41af90142a80f54918311b28e
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-24 12:42:19 +0530

    add undefine, null and BigInt

---
+++ 0 Basic/11_undefine_null_BigINT.js
@@ -0,0 +1,21 @@
+// undefine
+// null
+
+// let firstName;
+// console.log(typeof firstName, firstName);
+
+// firstName=0;
+// console.log(typeof firstName, firstName);
+
+let myVariable=null;
+console.log(typeof myVariable);
+console.log(myVariable);
+// console.log(typeof null); object but it will be null //bug , error 
+
+// bigInt
+let myNumber=BigInt(1232132546354321324521254563142354);
+let sameMyNumber=123n;
+console.log((myNumber));
+
+// console.log(Number.MAX_SAFE_INTEGER);
+console.log(myNumber+ sameMyNumber);
