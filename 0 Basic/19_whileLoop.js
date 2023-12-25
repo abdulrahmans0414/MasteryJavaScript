7eaf6d057dfe18554f2d4d53176adc3aa6855e52
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-25 12:55:05 +0530

    add while loop and a exmple of while loop

---
+++ 0 Basic/19_whileLoop.js
@@ -0,0 +1,27 @@
+// While loop
+
+// print 0 to 9 numbers
+
+// let i = 0; // 1 2 3
+// while (i <= 9) {
+//     console.log(i);
+//     i++;
+// }
+// console.log(`current value of i is $(i)`);
+
+// while loop example
+
+// sum of numbers 1 to 10
+let num = 10;
+let total = 0;
+let i = 0;
+
+while (i <= 10) {
+    total = total + i;
+    i++
+}
+console.log(total);
+
+// n*(n+1)/2 sum of natural numbers
+total = num * (num + 1) / 2;
+console.log(total);
