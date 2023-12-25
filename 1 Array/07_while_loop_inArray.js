f76c07bb7ab7afe529d43b481dd21b5fcf75cff7
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-25 22:05:40 +0530

    add while loop in array

---
+++ 1 Array/07_while_loop_inArray.js
@@ -0,0 +1,10 @@
+//  while loop in array 
+
+const fruits = ["apple", "mango", "grapes", "banana"];
+const fruits2 = [];
+let i = 0;
+while (i < fruits.length) {
+    fruits2.push(fruits[i].toUpperCase());
+    i++;
+}
+console.log(fruits2);
