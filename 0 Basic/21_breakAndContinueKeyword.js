aac03904c6302afe4538381474f00fee59e14d0c
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-25 18:03:24 +0530

    add break and continue keywords

---
+++ 0 Basic/21_breakAndContinueKeyword.js
@@ -0,0 +1,17 @@
+// break and continue keywords
+
+// Break keyword
+for (let i = 0; i <= 10; i++) {
+    if (i === 4) {
+        break;
+    }
+    console.log(i);
+}
+
+// continue Keyword
+for (let i = 0; i <= 10; i++) {
+    if (i === 4) {
+        continue;
+    }
+    console.log(i);
+}
