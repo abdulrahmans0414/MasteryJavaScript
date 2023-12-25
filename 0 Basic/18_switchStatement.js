7b5e75bf14fc31cdf9ed0a731e8ae4315504eba3
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-25 12:43:41 +0530

    add switch statement

---
+++ 0 Basic/18_switchStatement.js
@@ -0,0 +1,29 @@
+// swich Statement
+
+let day = 2;
+switch (day) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thrusday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Invalide day");
+        break;
+}
