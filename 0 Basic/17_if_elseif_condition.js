723324868f2c522c748bba90ccc8cad98e1a781e
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-25 12:40:06 +0530

    add if else if condition

---
+++ 0 Basic/17_if_elseif_condition.js
@@ -0,0 +1,26 @@
+// if
+// else if
+// else if
+// else if
+// else
+
+let tempInDegree = 50;
+
+if (tempInDegree < 0) {
+    console.log("extreme cold outside");
+
+} else if (tempInDegree < 16) {
+    console.log("Its is cold outside")
+}
+else if (tempInDegree < 25) {
+    console.log("Whether is okey")
+}
+else if (tempInDegree < 36) {
+    console.log("Lets go for swim")
+}
+else if (tempInDegree < 46) {
+    console.log("Turn on AC")
+}
+else {
+    console.log("too hot!!")
+}
