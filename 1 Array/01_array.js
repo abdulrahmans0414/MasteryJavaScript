cfbcb7f2e14a059198b5adc44d2ef7fb186ee45a
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-25 18:26:52 +0530

    add array folder and add file 01_array.js and index.html

---
+++ 1 Array/01_array.js
@@ -0,0 +1,22 @@
+// intro to array
+// refrence type
+// how to create array
+// orderded collection of items
+let fruit = ["apple", "Banana", "orange"];
+let mixed = [1, 2, 3, "string", "null", undefined];
+console.log(fruit);
+console.log(mixed);
+
+// array indexing
+fruit[1] = "mango";
+console.log(fruit);
+
+
+
+// array is object in java script
+console.log(typeof fruit); //object 
+console.log(Array.isArray(fruit));
+
+// object 
+let obj = {}; // object literal
+console.log(typeof obj);
