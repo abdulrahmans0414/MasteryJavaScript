676880d91e4548000d584219a2d663a1427f58ef
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-25 13:08:52 +0530

    add for loop with a example

---
+++ 0 Basic/20_for_loop.js
@@ -0,0 +1,17 @@
+// for loop
+
+// print 0 to 9 numbers
+for (let i = 0; i <= 9; i++) {
+    console.log(i);
+}
+console.log(i);  // is not work in outer loop
+
+
+// for loop exampe : sum of numbers 0 to 100
+let total = 0;
+let num = 100;
+
+for (let i = 1; i <= num; i++) {
+    total = total + i;
+}
+console.log(total);
