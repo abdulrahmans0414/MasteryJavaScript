fbf0102f6a4979ba90fca65b517286945f0c0f73
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-26 17:58:55 +0530

    add object folder and add file index.html and 01_object.js in which learn intro of objects

---
+++ 2 Objects/01_object.js
@@ -0,0 +1,22 @@
+// objects refrence type
+// arrays are goog but not suffucent
+// for real world data
+// objects store key value pairs
+// objects don't have index
+
+// how create objects
+const person = {
+    name: "Abdul RAhman",
+    age: 22,
+    hobbies: ["coding", "learning", "sleeping"]
+
+};
+console.log(person);
+
+// how to access data from objects
+console.log(person.name);
+console.log(person.hobbies);
+
+// how to add key value pairs to objects 
+person.gender = 'male';
+console.log(person);
