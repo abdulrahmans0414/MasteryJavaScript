f2807ddb6d443296f875e25ff5e320272c4eddac
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-29 22:20:53 +0530

    learn about if arrow function have only one parameter then we can remove () parathensis and also write in one line to remove aslo return .

--- 3 Functions/03_arrowFunction.js
+++ 3 Functions/03_arrowFunction.js
@@ -17,3 +17,8 @@ const findTarget = (array, target) => {
 const myArray = [1, 3, 5, 9, 27]
 const ans = findTarget(myArray, 9)
 console.log("Target value found at index :", ans);
+
+// example
+// if a function have only one parameter, then we can remove ()
+const isEven = number => number % 2 === 0;
+console.log(isEven(12));
