d83ded651767819dac94fbd54e463623806dc225
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-12-29 21:59:29 +0530

    add function expression

---
+++ 3 Functions/02_functionExpression.js
@@ -0,0 +1,13 @@
+// function expression
+
+// function declaration  -> 
+function writeMyName() {
+    console.log("Abdul Rahman");
+}
+writeMyName();
+
+// function declaration -> function expression
+const writeMyName = function () {
+    console.log("Abdul Rahman");
+}
+writeMyName(); // called function | run function | invoke function 
