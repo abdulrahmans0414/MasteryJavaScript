3684750871a46789cdb21d11f6858b07e581de2c
Abdul Rahman <abdulrahmans0414@gmail.com>
2024-01-11 15:47:51 +0530

    add another example of this method in createw own method file

--- 6 Oops/01_createOwnMethod.js
+++ 6 Oops/01_createOwnMethod.js
@@ -1,7 +1,7 @@
 // Method 
 // function inside object
 
-
+// example:
 const person = {
     firstName: 'Abdul',
     age: 22,
@@ -13,3 +13,33 @@ const person = {
 }
 console.log(person.about);
 person.about();
+
+// another example
+function userInfo() {
+    // this is object that call function
+    console.log(this);
+    console.log(`person name is ${this.firstName} and person age is ${this.age}`);
+}
+
+const user1 = {
+    firstName: 'Abdul',
+    age: 22,
+    about: userInfo
+}
+
+const user2 = {
+    firstName: 'Rehan',
+    age: 17,
+    about: userInfo
+}
+
+const user3 = {
+    firstName: 'Samar',
+    age: 16,
+    about: userInfo
+}
+console.log(userInfo);
+userInfo();
+user1.about();
+user2.about();
+user3.about();
