9b53396a761481ae74eeb7856ab3edc5f384abe7
Abdul Rahman <abdulrahmans0414@gmail.com>
2024-01-15 23:14:41 +0530

    add create a function that function creats a object

--- 6 Oops/06_Oops.js
+++ 6 Oops/06_Oops.js
@@ -41,4 +41,38 @@ console.log(aboutUser2);
 console.log(user2Is18);
 
 
+// if we need 10M users !!, then we need to better approach
+// thatwhy we create a function that take as input -> firstName, lastName, email, age, address and return a object.
 
+// function (that function creates a object)
+// 2.) add key value pair
+// 3.) return that object
+
+// for example -> 
+// Its work proper but it's bad practice - 
+function createUser(firstName, lastName, email, age, address) {
+    const user = {};
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+
+    // function inside object call method
+    user.about = function () {
+        return `${this.firstName} ${this.lastName} is ${this.age} years old.`
+    }
+    user.is18 = function () {
+        return this.age >= 18;
+    }
+    return user;
+}
+
+const user3 = createUser('Abdul', 'Rahman', 'iamarsiddiqui@gmail.com', 22, 'lucknow');
+console.log(user3);
+
+// called method
+const aboutUser3 = user3.about();
+const is18User3 = user3.is18();
+console.log(aboutUser3);
+console.log(is18User3);
