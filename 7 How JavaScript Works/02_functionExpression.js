eb4a49cc9a81d7d8ac11137c6dfce35d46e33f22
Abdul Rahman <abdulrahmans0414@gmail.com>
2024-01-25 08:39:19 +0530

    how js work in case of function expression

---
+++ 7 How JavaScript Works/02_functionExpression.js
@@ -0,0 +1,10 @@
+// how javascript work in case of function expression
+
+
+console.log(myFunction); // undefine
+
+var myFunction = function () {
+    console.log("this is my function");
+}
+
+console.log(myFunction); 
