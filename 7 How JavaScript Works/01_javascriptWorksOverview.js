28d196782e12764d87fbb19d0d96eed7b4a67de9
Abdul Rahman <abdulrahmans0414@gmail.com>
2024-01-25 08:27:39 +0530

    add how to javascript execute code, global execution context and what happens to function declarations

---
+++ 7 How JavaScript Works/01_javascriptWorksOverview.js
@@ -0,0 +1,39 @@
+// compilation
+// code execution
+
+// why compilation
+
+
+// how javascript code execution
+
+
+// what is global execution context ?
+// what is local execution context  ?
+
+// closures
+
+
+// for example 1
+console.log(this);
+console.log(window);
+
+console.log(firstName);
+var firstName = "Abdul";
+console.log(firstName);
+
+
+// example 2
+console.log(this);
+console.log(window);
+console.log(myFunc);
+console.log(fullName);
+
+// function declarations
+function myFunc() {
+    console.log("This is my function");
+}
+
+var firstName = "Abdul";
+var lastName = "Rahman";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
