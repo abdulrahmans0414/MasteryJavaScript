f9f39253a3a4fbb656067131f192cb1312ca3ee5
Abdul Rahman <abdulrahmans0414@gmail.com>
2024-01-25 08:55:09 +0530

    add hjw to are let and const are hoisting

---
+++ 7 How JavaScript Works/03_AreHoisted.js
@@ -0,0 +1,22 @@
+// Are let and const are hoisting
+
+// In case of let variable
+// hoisting
+
+// Uncaught ReferenceError: 
+// Cannot access 'firstName' before initialization
+
+// Uncaught ReferenceError: 
+// firstName is not defined
+
+// console.log(firstName);  // unintialization / if not create varible and called then give not define
+let firstName = "Abdul";
+console.log(firstName); // assigned undefine to Abdul
+
+
+// In case of const variable
+// hoisting
+
+// console.log(lastName); 
+const lastName = "Rahman";
+console.log(lastName);
