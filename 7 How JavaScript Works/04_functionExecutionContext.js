5789d9a82236304b2cea890ee53f02a209b958d4
Abdul Rahman <abdulrahmans0414@gmail.com>
2024-01-26 17:01:36 +0530

    add function execution context

---
+++ 7 How JavaScript Works/04_functionExecutionContext.js
@@ -0,0 +1,12 @@
+//  how to work function execution context
+
+console.log("hello world");
+let firstName = "Abdul";
+let lastName = "Rahman";
+
+const myFunction = function () {
+    let var1 = "First Variable";
+    let var2 = "second Variable";
+    console.log(var1);
+    console.log(var2);
+}
