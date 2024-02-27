4a3b95a6d717e988e7ff64dc248bbd495d65f630
Abdul Rahman <abdulrahmans0414@gmail.com>
2024-02-27 14:25:15 +0530

    add new directory Asynchronous js and Xml and add intro ajax

---
+++ Asynchronously JS and XML/01_Intro_AJAX.js
@@ -0,0 +1,24 @@
+// Basic Theory
+
+// AJAX :  asynchronous javascript and XML
+
+// HTTP request
+
+// is a set of "web development techniques"
+// using many web technologies on the "client-side"
+// to create asynchronous web applications
+
+
+// with Ajax, web applications can send and retrieve
+// data from the server asynchronously (in the background)
+// without interfering with the display and
+// behavior of the existing page
+
+// we don't  use data in XML format anymore
+// we use JSON now.
+
+
+// we have 3 most common ways to create and send request to the server
+// 1.) xmlHTTPRequest (old way of doing)
+// 2.) fetch API (new way of doing)
+// 3.) axios (this is third party library)
